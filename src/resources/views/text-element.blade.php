0ea4b937c582e61b7f463dd115d4e721ad7979dc
Florian Heinrich <heinrich@convidera.com>
2019-07-15 10:16:58 +0200

    add additionalAttributes and -Classes to text

--- src/resources/views/text-element.blade.php
+++ src/resources/views/text-element.blade.php
@@ -1,13 +1,26 @@
 @if(!Auth::user() || !$options->changeable || empty($data))
 
-    {!! empty($options->tag) ? '' : "<$options->tag>" !!}
+    @if(!empty($options->tag))
+        <{{ $options->tag }}
+            @if(!empty($options->additionalClasses))
+                class="{{ $options->additionalClasses }}"
+            @endif
+            @if(!empty($options->additionalAttributes))
+                {!! $options->additionalAttributes !!}
+            @endif
+        >
+    @endif
+
         {{ empty($data) ? '' : $data->value }}
-    {!! empty($options->tag) ? '' : "</$options->tag>" !!}
+
+    @if(!empty($options->tag) && $options->closeTag)
+        </{{ $options->tag }}>
+    @endif
 
 @else
 
     <{{ empty($options->tag) ? 'span' : "$options->tag" }}
-        class="WYSIWYG__container WYSIWYG__container-text WYSIWYG__container-text-plain"
+        class="WYSIWYG__container WYSIWYG__container-text WYSIWYG__container-text-plain {{ $options->additionalClasses }}"
         data-id="{{ $data->id }}"
         data-key="{{ $data->key }}"
         data-element-type="text"
@@ -15,8 +28,16 @@
         data-value-origin="{{ $data->value }}"
         data-value-saved="{{ $data->value }}"
         data-placeholder="{{env('DISPLAY_TEXT_ELEMENT_KEYS', false) ? '>>' . $data->key . '<<' : ''}}"
+
+        @if(!empty($options->additionalAttributes))
+            {!! $options->additionalAttributes !!}
+        @endif
     >
+
         {{ $data->value }}
-    </{{ empty($options->tag) ? 'span' : "$options->tag" }}>
+
+    @if(empty($options->tag) || $options->closeTag)
+        </{{ empty($options->tag) ? 'span' : "$options->tag" }}>
+    @endif
 
 @endif
