21605b05dba6cc7231f59469f025ecba433369a4
valentin <brueggemann@convidera.com>
2019-07-17 10:24:00 +0200

    text element types

--- src/app/Nova/TextElement.php
+++ src/app/Nova/TextElement.php
@@ -3,10 +3,13 @@
 namespace Convidera\WYSIWYG\Nova;
 
 use Convidera\WYSIWYG\Nova\Resource;
+use Epartment\NovaDependencyContainer\NovaDependencyContainer;
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Markdown;
 use Laravel\Nova\Fields\MorphTo;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Fields\Select;
 use Wehaa\Liveupdate\Liveupdate;
 
 abstract class TextElement extends Resource
@@ -64,7 +67,22 @@ abstract class TextElement extends Resource
     {
         return [
             Text::make('Key')->sortable(),
-            Textarea::make('Value'),
+            Select::make('type', 'type')->options([
+                'plain' => 'plain',
+                'markdown' => 'markdown',
+            ])->fillUsing(function ($request, $model, $attribute, $requestAttribute) {
+                return;
+            })->displayUsingLabels(),
+
+
+            NovaDependencyContainer::make([
+                Textarea::make('Value'),
+            ])->dependsOn('type', 'plain'),
+
+            NovaDependencyContainer::make([
+                Markdown::make('Value'),
+            ])->dependsOn('type', 'markdown'),
+
             Liveupdate::make('Value')->onlyOnIndex(),
             MorphTo::make('TextElementable')->types($this->getTextElementableTypes()),
         ];
